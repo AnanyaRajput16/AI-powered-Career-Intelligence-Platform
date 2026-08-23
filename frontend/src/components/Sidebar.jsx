984eff5cc201f7eadbd926c45b7c8280686a5703
Ananya Rajput <2k23cs2313737@gmail.com>
2026-08-23 15:19:52 +0530

    Add Resume Parser to user sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -50,7 +50,16 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
         </svg>
       )
-    },
+    }, 
+    {
+      name: 'Resume Parser',
+      path: '#resume-parsing',
+      icon: (
+        <svg fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
+        </svg>
+    )
+   },
     {
       name: 'ATS Analysis',
       path: '#ats-analysis',
